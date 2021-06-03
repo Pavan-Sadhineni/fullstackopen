d799e977fd44b5731017e428cbeddb3d8baf4d44
Pavan-bit-ui <72428550+Pavan-bit-ui@users.noreply.github.com>
2021-06-03 15:25:30 +0530

    Part 3 Done

---
+++ part3/Phone-Backend/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const nameInput = process.argv[3]
+const numberInput = process.argv[4]
+
+
+const url = `mongodb+srv://fullstack:Pavan9949@cluster0.gwqkb.mongodb.net/test?retryWrites=true&w=majority`
+
+mongoose.connect(url, {
+    useNewUrlParser: true
+})
+
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: nameInput,
+    number: numberInput
+})
+
+if ((nameInput != undefined) && (numberInput != undefined)) {
+    person.save().then(result => {
+        console.log(`lisätään ${nameInput} numero ${numberInput} luetteloon`)
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name + " " + person.number)
+        })
+        mongoose.connection.close()
+    })
+}
