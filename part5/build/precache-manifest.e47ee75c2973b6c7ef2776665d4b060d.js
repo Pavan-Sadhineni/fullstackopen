f73b395f8597d8be597aa8a392313e74f22cb1a0
Sadhineni Pavan Kumar <72428550+Pavan-bit-ui@users.noreply.github.com>
2021-06-07 07:37:35 +0530

    part 5 done

---
+++ part5/build/precache-manifest.e47ee75c2973b6c7ef2776665d4b060d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71d7128ebb1fea3b9b00273febf2724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18cf2eac2aac3643cd07",
+    "url": "/static/css/main.dcbbdc42.chunk.css"
+  },
+  {
+    "revision": "97f7af5aad7fb5a39aa8",
+    "url": "/static/js/2.06b675a4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.06b675a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18cf2eac2aac3643cd07",
+    "url": "/static/js/main.2f8f9473.chunk.js"
+  },
+  {
+    "revision": "038c38d3471e45bc1ed6",
+    "url": "/static/js/runtime-main.63d0d90a.js"
+  }
+]);
