861406d4298ae22cd5792c87ac67c25f8dd61774
Sadhineni Pavan Kumar <72428550+Pavan-bit-ui@users.noreply.github.com>
2021-06-10 16:14:22 +0530

    part7 done

---
+++ part7/bloglist-backend/build/precache-manifest.14f90bb4840c71b6ff2936681f3965a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea06bc82c63807ef8f0dbfd2036fdcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77d9878f33fe4a8e2d67",
+    "url": "/static/css/main.064b7281.chunk.css"
+  },
+  {
+    "revision": "a762eaca2a876757a14a",
+    "url": "/static/js/2.9f08a5cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f08a5cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d9878f33fe4a8e2d67",
+    "url": "/static/js/main.ecf79a99.chunk.js"
+  },
+  {
+    "revision": "39de2ec51c925436d1b1",
+    "url": "/static/js/runtime-main.03b35062.js"
+  }
+]);
